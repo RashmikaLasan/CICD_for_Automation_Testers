aa8893802f10f0d4c2d453c0c3c2c42cb39b270d
RashmikaLasan <lasanrash@gmail.com>
2022-02-07 02:35:40 +0530

    lasan0020- After Method Comment

--- src/test/java/package1/TestClass1.java
+++ src/test/java/package1/TestClass1.java
@@ -39,7 +39,7 @@ public class TestClass1 {
     public void Test3() {
 
         driver.quit();
-        System.out.println("Lasan001 the Browser");
+        System.out.println("Lasan002 the Browser");
     }
 
 }
